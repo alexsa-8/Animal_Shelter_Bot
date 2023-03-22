59f021a9162103bb084a7d40896cb7f6bd0d7655
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-03-22 20:14:05 +0300

    correct test #2

--- src/main/java/pro/sky/animalshelterbot/AnimalShelterBotApplication.java
+++ src/main/java/pro/sky/animalshelterbot/AnimalShelterBotApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AnimalShelterBotApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(AnimalShelterBotApplication.class, args);
     }
 
