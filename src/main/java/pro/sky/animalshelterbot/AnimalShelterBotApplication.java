8352391fb8634ef977642347334c7fdc514bccc4
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-03-22 19:51:13 +0300

    start project

---
+++ src/main/java/pro/sky/animalshelterbot/AnimalShelterBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.animalshelterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalShelterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/animalshelterbot/AnimalShelterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.animalshelterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
