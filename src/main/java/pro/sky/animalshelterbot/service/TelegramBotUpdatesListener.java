ee38b5ad6f4927f4630d3a6ae3b13941603eaac8
Ilya <b.ilya.98@mail.ru>
2023-03-27 12:59:32 +0300

    Divided all commands into methods, created a menu

--- src/main/java/pro/sky/animalshelterbot/service/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animalshelterbot/service/TelegramBotUpdatesListener.java
@@ -2,8 +2,10 @@ package pro.sky.animalshelterbot.service;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SetMyCommands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,11 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
+        telegramBot.execute(new SetMyCommands(
+                new BotCommand("/start", "Get a welcome message"),
+                new BotCommand("/info", "Get detailed information on all the features of the bot"),
+                new BotCommand("/volunteer", "Call a volunteer")
+        ));
     }
 
 
@@ -30,28 +37,51 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-            SendMessage greeting = new SendMessage(update.message().chat().id(),
-                    "Привет, " + update.message().from().firstName() + "! \uD83D\uDE42");
-
-            String infoMsg = "«Приют» — слово, от которого становится тоскливо.\uD83D\uDE14" +
-                    "«Приют для животных» звучит еще более безрадостно.\uD83D\uDE22" +
-                    "Это место, где живут брошенные питомцы, потерявшиеся при переезде, пережившие своих хозяев или рожденные на улице. \uD83C\uDF27" +
-                    "\n" +
-                    "Поначалу животные в приютах ждут\uD83D\uDC15, что за ними вернутся старые владельцы. \uD83D\uDC64" +
-                    "Потом они ждут своих друзей-волонтеров \uD83D\uDC71\uD83C\uDFFB\u200D♂️ \uD83D\uDC71\uD83C\uDFFB\u200D♀️, " +
-                    "корм по расписанию⌛, посетителей, которые погладят и почешут за ухом.❤️ " +
-                    "Но больше всего приютские подопечные ждут, что их заберут домой.\uD83C\uDFE0 " +
-                    "Если вы решили взять питомца из приюта, то мы с радостью расскажем, как это сделать!\uD83D\uDC4D";
-            SendMessage info = new SendMessage(update.message().chat().id(), infoMsg);
             String message = update.message().text();
 
-            logger.info("Processing update: {}", update);
-
             if (message != null && update.message().text().equals("/start")) {
-                telegramBot.execute(greeting);
-                telegramBot.execute(info);
+                greeting(update);
+                info(update);
+            } else if (update.message().text().equals("/info")) {
+                info(update);
+            } else if (update.message().text().equals("/volunteer")) {
+                volunteer(update);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+
+    // method sends a greeting to the user
+    public void greeting(Update update) {
+        logger.info("Greeting to " + update.message().text());
+        SendMessage greeting = new SendMessage(update.message().chat().id(),
+                "Привет, " + update.message().from().firstName() + "! \uD83D\uDE42");
+
+        telegramBot.execute(greeting);
+    }
+
+    // method sends information to the user
+    public void info(Update update) {
+        logger.info("Info to " + update.message().text());
+        String infoMsg = "«Приют» — слово, от которого становится тоскливо.\uD83D\uDE14" +
+                "«Приют для животных» звучит еще более безрадостно.\uD83D\uDE22" +
+                "Это место, где живут брошенные питомцы, потерявшиеся при переезде, пережившие своих хозяев или рожденные на улице. \uD83C\uDF27" +
+                "\n" +
+                "Поначалу животные в приютах ждут\uD83D\uDC15, что за ними вернутся старые владельцы. \uD83D\uDC64" +
+                "Потом они ждут своих друзей-волонтеров \uD83D\uDC71\uD83C\uDFFB\u200D♂️ \uD83D\uDC71\uD83C\uDFFB\u200D♀️, " +
+                "корм по расписанию⌛, посетителей, которые погладят и почешут за ухом.❤️ " +
+                "Но больше всего приютские подопечные ждут, что их заберут домой.\uD83C\uDFE0 " +
+                "Если вы решили взять питомца из приюта, то мы с радостью расскажем, как это сделать!\uD83D\uDC4D";
+        SendMessage info = new SendMessage(update.message().chat().id(), infoMsg);
+
+        telegramBot.execute(info);
+    }
+
+    // The method calls the volunteer
+    public void volunteer(Update update) {
+        logger.info("volunteer to " + update.message().text());
+        SendMessage volunteer = new SendMessage(update.message().chat().id(), "Волонтер скоро с вами свяжется\uD83D\uDE09");
+
+        telegramBot.execute(volunteer);
     }
 }
